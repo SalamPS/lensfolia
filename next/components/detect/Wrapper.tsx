63a5d3d6c1179dfca04bf161f473f1704c9102db
SalamPS <salamp@salamp.id>
2025-06-04 06:40:09 +0700

    chore(server): Added prefix

--- next/components/detect/Wrapper.tsx
+++ next/components/detect/Wrapper.tsx
@@ -1,3 +1,4 @@
+"use server"
 export default function LFDWrapper({children}: {children: React.ReactNode}) {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-background gap-14 font-[family-name:var(--font-poppins)]">
