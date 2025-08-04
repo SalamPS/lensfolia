5787f5e8eb19f53c7a91f400d0efb3f52a29186f
Nazlan Rizqon <nazlanrizqon@gmail.com>
2025-08-04 20:07:59 +0700

    fix: minor (styling and layouting)

--- next/components/detect/AskAI.tsx
+++ next/components/detect/AskAI.tsx
@@ -11,35 +11,35 @@ const AIBubble = ({ text }: { text: string }) => {
 
 export function AskAI ({disease}: {disease: LFDResult_ | null}) {
 	return (
-		<div id="ask-ai" className="border-border rounded-3xl border-[1px]">
-			<h3 className="border-border flex items-center justify-center border-b-[1px] p-2 py-6 font-semibold">
-				<IconSparkles className="mr-2 inline" size={24} />
-				Konsultasi lebih lanjut dengan AI
-			</h3>
-			<div className="flex flex-col gap-4 px-6">
-				<div className="flex h-80 grow flex-col items-center justify-center">
-					<h4 className="mb-8 font-semibold">
-						Apa yang bisa saya bantu?
-					</h4>
-					<div className="w-[80%] text-center">
-						{disease?.ai_bubble?.length
-							? disease.ai_bubble.map((text, index) => (
-									<AIBubble key={index} text={text} />
-								))
-							: null}
-					</div>
-				</div>
-				<div className="bg-secondary mt-2 flex rounded-t-2xl p-3 px-5">
-					<input
-						className="text-muted-foreground grow outline-none"
-						type="text"
-						placeholder="Tanyakan apapun terkait deteksi daunmu"
-					/>
-					<button className="bg-foreground text-background hover:bg-primary/10 rounded-full p-3 transition-colors">
-						<IconSend />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+    <div id="ask-ai" className="border-border rounded-3xl border-[1px]">
+      <h3 className="border-border text-foreground flex items-center justify-center border-b-[1px] p-2 py-6 font-semibold">
+        <IconSparkles className="mr-2 inline" size={24} />
+        Konsultasi lebih lanjut dengan AI
+      </h3>
+      <div className="flex flex-col gap-4 px-6">
+        <div className="flex h-80 grow flex-col items-center justify-center">
+          <h4 className="text-foreground mb-8 font-semibold">
+            Apa yang bisa saya bantu?
+          </h4>
+          <div className="w-[80%] text-center">
+            {disease?.ai_bubble?.length
+              ? disease.ai_bubble.map((text, index) => (
+                  <AIBubble key={index} text={text} />
+                ))
+              : null}
+          </div>
+        </div>
+        <div className="bg-secondary mt-2 flex rounded-t-2xl p-3 px-5">
+          <input
+            className="text-foreground grow outline-none"
+            type="text"
+            placeholder="Tanyakan apapun terkait deteksi daunmu"
+          />
+          <button className="bg-foreground text-background hover:bg-primary/10 rounded-full p-3 transition-colors">
+            <IconSend />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- next/components/detect/Result.tsx
+++ next/components/detect/Result.tsx
@@ -106,27 +106,31 @@ export default function LFDResultPage({detId}: {detId?: string}) {
   };
   return (
     <LFDWrapper>
-      <section
-        id="preview-image"
-        className="border-border w-fullbg-card/[0.12] border-[2px] border-dashed p-6 backdrop-blur-md"
-      >
-        <div className="text-foreground mb-6 flex items-center gap-4 text-sm font-semibold">
-          <Button
-            className="dark:bg-secondary/40 dark:hover:bg-secondary flex h-10 w-10 cursor-pointer rounded-full bg-zinc-200 transition-colors hover:bg-zinc-300"
-            onClick={() => {
-              router.back();
-            }}
-          >
-            <IconArrowLeft className="text-foreground" />
-          </Button>
-          {result?.image_url}
-        </div>
-        <div className="border-border bg-card flex flex-col items-center justify-center gap-1 overflow-hidden rounded-[20px] border-[1px] p-4">
-          <img
-            src={result?.image_url || "/placeholder.svg"}
-            alt="Captured Image"
-            className="max-w-md rounded-md object-cover"
-          />
+      <section className="flex w-full items-center justify-center px-4">
+        <div
+          id="preview-image"
+          className="border-border bg-card/[0.12] w-full border-[2px] border-dashed p-4 backdrop-blur-md sm:max-w-[500px] sm:p-6"
+        >
+          <div className="text-foreground mb-4 flex items-center gap-2 text-xs font-semibold sm:mb-6 sm:gap-4 sm:text-sm">
+            <Button
+              className="dark:bg-secondary/40 dark:hover:bg-secondary flex h-8 w-8 cursor-pointer rounded-full bg-zinc-200 transition-colors hover:bg-zinc-300 sm:h-10 sm:w-10"
+              onClick={() => {
+                router.back();
+              }}
+            >
+              <IconArrowLeft className="text-foreground text-sm sm:text-base" />
+            </Button>
+            <div className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+              {result?.image_url}
+            </div>
+          </div>
+          <div className="border-border bg-card flex flex-col items-center justify-center gap-1 overflow-hidden rounded-lg border-[1px] p-2 sm:rounded-[20px] sm:p-4">
+            <img
+              src={result?.image_url || "/placeholder.svg"}
+              alt="Captured Image"
+              className="w-full max-w-xs rounded-md object-cover sm:max-w-md"
+            />
+          </div>
         </div>
       </section>
 
@@ -134,9 +138,11 @@ export default function LFDResultPage({detId}: {detId?: string}) {
       <section id="disease-result" className="z-20">
         <h1 className="dark:from-primary mt-8 mb-4 bg-gradient-to-b from-zinc-500 to-zinc-700 bg-clip-text text-center text-4xl font-bold text-transparent dark:to-emerald-800">
           {!result?.diagnoses_result.length ? (
-            <span className="">Tidak ada penyakit atau hama yang terindikasi!</span>
-          ) : (
             <span className="">
+              Tidak ada penyakit atau hama yang terindikasi!
+            </span>
+          ) : (
+            <span className="w-full px-4 text-center">
               Ditemukan {result.diagnoses_result.length} indikasi!
             </span>
           )}
@@ -144,20 +150,21 @@ export default function LFDResultPage({detId}: {detId?: string}) {
 
         {result?.diagnoses_result.length ? (
           <div className="my-14">
-            <div className="mx-auto max-w-2xl">
-              <Carousel setApi={setApi} className="w-[400px]">
+            <div className="flex justify-center px-4">
+              <Carousel setApi={setApi} className="w-fit max-w-[350px]">
                 <CarouselContent>
                   {result.diagnoses_result.map((disease, index) => (
                     <CarouselItem key={index}>
-                      <Card className="border-border">
+                      <Card className="border-border w-full">
                         <CardContent className="flex flex-col items-center justify-center">
                           <img
                             src={disease.image_url || "/placeholder.svg"}
                             alt={disease.encyclopedia?.name || "Disease Image"}
-                            className="mb-4 w-full rounded-md object-cover"
+                            className="mb-4 aspect-[4/3] min-h-[200px] w-full rounded-md object-cover"
                           />
                           <h2 className="my-2 text-lg font-semibold">
-                            {disease.encyclopedia?.name || "Penyakit Tidak Diketahui"}
+                            {disease.encyclopedia?.name ||
+                              "Penyakit Tidak Diketahui"}
                           </h2>
                           <p
                             className={`text-foreground mt-1 mb-2 rounded-full border-[1px] p-2 px-4 text-xs font-semibold ${
@@ -168,8 +175,8 @@ export default function LFDResultPage({detId}: {detId?: string}) {
                                   : "bg-destructive/30 border-destructive"
                             }`}
                           >
-                            Tingkat Keyakinan:{" "}
-                            {Math.round(disease.score * 100)}%
+                            Tingkat Keyakinan: {Math.round(disease.score * 100)}
+                            %
                           </p>
                         </CardContent>
                       </Card>
@@ -193,7 +200,9 @@ export default function LFDResultPage({detId}: {detId?: string}) {
                     key={index}
                     onClick={() => api?.scrollTo(index)}
                     className={`h-2 w-2 rounded-full transition-colors ${
-                      current === index + 1 ? "bg-primary" : "bg-muted"
+                      current === index + 1
+                        ? "bg-primary"
+                        : "dark:bg-muted bg-zinc-300"
                     }`}
                     aria-label={`Go to slide ${index + 1}`}
                   />
@@ -223,7 +232,7 @@ export default function LFDResultPage({detId}: {detId?: string}) {
           id="discussion"
           className="bg-card z-20 mx-4 my-16 flex w-full flex-col gap-4 rounded-4xl p-8 px-4 pb-4 md:max-w-7xl"
         >
-          <h2 className="mb-4 text-center text-2xl font-semibold">
+          <h2 className="text-foreground mb-4 text-center text-2xl font-semibold">
             Pembahasan Terkait{" "}
             {currentDisease?.encyclopedia?.name || "Penyakit Tanaman"}
           </h2>
@@ -261,7 +270,7 @@ export default function LFDResultPage({detId}: {detId?: string}) {
             lebih lanjut, konsultasikan dengan ahli pertanian atau dokter
             tanaman.
           </p>
-          <AskAI disease={currentDisease}/>
+          <AskAI disease={currentDisease} />
         </div>
       </section>
     </LFDWrapper>
@@ -285,7 +294,7 @@ const Discussion: React.FC<DiscussionProps> = ({
         <div className="border-border bg-card mr-3 flex items-center justify-center rounded-full border-[1px] p-3 shadow-2xl">
           {children}
         </div>
-        <h3 className="text-lg font-semibold">{title}</h3>
+        <h3 className="text-lg font-semibold text-foreground">{title}</h3>
       </div>
       <p className="text-muted-foreground">{description}</p>
     </div>
