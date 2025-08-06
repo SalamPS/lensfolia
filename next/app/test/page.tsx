4e666b49959c6bebef9c56d0114717f0b7903e72
SalamPS <salamp@salamp.id>
2025-08-06 22:22:57 +0700

    feat(debug): Added navbar

--- next/app/test/page.tsx
+++ next/app/test/page.tsx
@@ -3,13 +3,15 @@
 import { OfflineTest } from "@/components/pwa/OfflineTest";
 import { ServiceWorkerDebug } from "@/components/pwa/ServiceWorkerDebug";
 import { LangGraphTestBox } from "@/components/lang/Tester";
+import Navbar from "@/components/home/Navbar";
 
 export default function App() {
   
 
   return (
     <div>
-      <div className="flex items-center justify-between bg-primary/60 p-4 text-white">
+      <Navbar/>
+      <div className="flex pt-20 items-center justify-between bg-primary/60 p-4 text-white">
         <h1 className="text-2xl font-bold">LensFolia Debug Page</h1>
       </div>
       <div className="flex flex-col justify-center bg-background p-18 px-72">
