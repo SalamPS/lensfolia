fc459928b0b2ba1c0e0deee91ebba19e4266e918
SalamPS <salamp@salamp.id>
2025-08-09 10:57:58 +0700

    merge(all): One piece

--- service/db/delete_expired_diagnoses.sh
+++
@@ -1,3 +0,0 @@
-curl -L -X POST 'https://ucofsfjumfhpuhnptaro.supabase.co/functions/v1/node-api' \
-  -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjb2ZzZmp1bWZocHVobnB0YXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwMjExMTcsImV4cCI6MjA2OTU5NzExN30.hWtkPTcug9xuKFzOdWRCTqeJaSDkFKRFy5LrqjBXKys' \ 
-  --data '{"name":"Functions"}'
